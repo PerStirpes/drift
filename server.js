533720a57d485f5b040db1935c1ea763d1d5bbea
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-07-19 22:51:26 -0700

    Keep calm and commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const path = require('path')
+const favicon = require('serve-favicon')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+const app = express()
 const Drift = require('drift-chat')
 
+app.use(cors())
+app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')))
 app.use(bodyParser.json())
-app.listen(process.env.PORT || 3000, () => console.log('Your bot is listening'))
 
 app.get('/', async (request, response) => {
   return response.status(200).send('check it')
@@ -15,5 +20,19 @@ app.post('/', (req, res) => {
   if (!data) return res.send(400)
 })
 
-//  ./ngrok http 3000
-// Q8KmS0RdE8ITvAHAMEb384FQOxukjU2n
+app
+  .listen(port, () => {
+    console.log('Your bot is listening')
+  })
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+
+// function formatError(err) {
+//   return {
+//     code: err.code,
+//     message: err.message,
+//     stack: err.stack,
+//   }
+// }
